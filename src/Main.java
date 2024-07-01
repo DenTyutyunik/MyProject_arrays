7ee222df7bca68a5f8dfdd080e23645f6896de60
DenTyutyunik <den.tyutyunik@gmail.com>
2024-07-01 22:50:41 +0400

    homework was revised

--- src/Main.java
+++ src/Main.java
@@ -12,64 +12,50 @@ public class Main {
         System.out.println();
         System.out.println("Task 2");
         System.out.println("intArray:");
-        for (int index = 0; index < intArray.length; index++) {
-            if (index == intArray.length - 1) {
-                System.out.println(intArray[index]);
-                break;
-            }
+        for (int index = 0; index < intArray.length - 1; index++) {
             System.out.print(intArray[index] + ", ");
         }
-
+        System.out.print(intArray[intArray.length - 1]);
+        System.out.println();
         System.out.println("doubleArray:");
-        for (int index = 0; index < doubleArray.length; index++) {
-            if (index == doubleArray.length - 1) {
-                System.out.println(doubleArray[index]);
-                break;
-            }
+        for (int index = 0; index < doubleArray.length - 1; index++) {
             System.out.print(doubleArray[index] + ", ");
         }
-
+        System.out.print(doubleArray[doubleArray.length - 1]);
+        System.out.println();
         System.out.println("charArray:");
-        for (int index = 0; index < charArray.length; index++) {
-            if (index == charArray.length - 1) {
-                System.out.println(charArray[index]);
-                break;
-            }
+        for (int index = 0; index < charArray.length - 1; index++) {
             System.out.print(charArray[index] + ", ");
         }
+        System.out.print(charArray[charArray.length - 1]);
+        System.out.println();
 
         // Task 3
         System.out.println();
         System.out.println("Task 3");
         System.out.println("intArray:");
-        for (int index = intArray.length - 1; index >= 0; index--) {
-            if (index == 0) {
-                System.out.println(intArray[index]);
-                break;
-            }
+        for (int index = intArray.length - 1; index > 0; index--) {
             System.out.print(intArray[index] + ", ");
         }
+        System.out.print(intArray[0]);
+        System.out.println();
         System.out.println("doubleArray:");
-        for (int index = doubleArray.length - 1; index >= 0; index--) {
-            if (index == 0) {
-                System.out.println(doubleArray[index]);
-                break;
-            }
+        for (int index = doubleArray.length - 1; index > 0; index--) {
             System.out.print(doubleArray[index] + ", ");
         }
+        System.out.print(doubleArray[0]);
+        System.out.println();
         System.out.println("charArray:");
-        for (int index = charArray.length - 1; index >= 0; index--) {
-            if (index == 0) {
-                System.out.println(charArray[index]);
-                break;
-            }
+        for (int index = charArray.length - 1; index > 0; index--) {
             System.out.print(charArray[index] + ", ");
         }
+        System.out.print(charArray[0]);
+        System.out.println();
 
         // Task 4
         System.out.println();
         System.out.println("Task 4");
-        for (int index = 0; index <= intArray.length -1; index++) {
+        for (int index = 0; index < intArray.length; index++) {
             if (intArray[index] % 2 == 1) {
                 intArray[index] += 1;
             }
